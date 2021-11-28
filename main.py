ea535db314751c12cd6fe21aaa1dc3b58df66060
khadada <lkhadada@gmail.com>
2021-11-28 17:29:02 +0100

    I used pandas library to the same project with minimal lines of code

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-import csv
-from prettytable import PrettyTable
-table = PrettyTable()
-
-with open("weather-data.csv") as data:
-    info = csv.reader(data)
-    temptures = []
-    for row in info:
-        if row[0] == 'day':
-            table.field_names = row
-        else:
-            table.add_row(row)
-    table.align = 'l'
-print(table)
+import pandas
+data = pandas.read_csv('weather-data.csv')
+print(data)
